20594dfe51fcfa74702cd82f6d8af58bef59044f
bailangvvkg <bailangvvking@outlook.com>
2024-12-29 22:55:58 +0800

    Update Packages.sh
    拉取luci-app-lucky源码

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -81,3 +81,6 @@ UPDATE_VERSION() {
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
 UPDATE_VERSION "sing-box"
 UPDATE_VERSION "tailscale"
+
+# 拉取Lucky最新版的源码
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
