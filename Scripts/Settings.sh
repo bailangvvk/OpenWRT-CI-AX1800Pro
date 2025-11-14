77984ae9629ccb38816b905faa4b12084f583997
bailangvvk <94160310+bailangvvk@users.noreply.github.com>
2025-11-14 14:41:38 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -108,3 +108,5 @@ echo "CONFIG_DEFAULT_tcp_cubic=y" >> ./.config
 echo "CONFIG_PACKAGE_nginx-mod-luci=y" >> ./.config
 # Nginx的图形化界面
 echo "CONFIG_PACKAGE_luci-app-nginx=y" >> ./.config
+# 让data时间精确到纳秒毫秒计算
+echo "CONFIG_PACKAGE_coreutils-date=y" >> ./.config
